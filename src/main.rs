d3e6002b660200282911a2f69a2c8be67e5bcdb7
Name <namesexistsinemails@gmail.com>
2023-10-27 21:51:31 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,42 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+
+use eframe::egui;
+
+fn main() {
+    render_app();
+}
+
+fn render_app() -> Result<(), eframe::Error> {
+    //  env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
+
+    let options = eframe::NativeOptions {
+        initial_window_size: Some(egui::vec2(240.0, 120.0)),
+        ..Default::default()
+    };
+
+    // Our application state:
+    let mut name = "Arthur".to_owned();
+    let mut age = 42;
+
+    eframe::run_simple_native("Kaemoji board", options, move |ctx, _frame| {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Kaemoji board");
+
+            egui::ScrollArea::vertical().show(ui, |ui| {
+                ui.horizontal(|ui| {
+                    let name_label = ui.label("Your name: ");
+                    ui.text_edit_singleline(&mut name)
+                        .labelled_by(name_label.id);
+                });
+                ui.add(egui::Slider::new(&mut age, 0..=120).text("age"));
+                let var = [0, 1, 2];
+                for i in var {
+                    if ui.button(var[i].to_string()).clicked() {
+                        age += var[i];
+                    }
+                }
+                ui.label(format!("Hello '{name}', age {age}"));
+            });
+        });
+    })
+}
