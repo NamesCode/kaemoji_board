3cd45ab37c687b50fd320b13704b4f7d6952f79c
Name <namesexistsinemails@gmail.com>
2023-11-19 18:05:20 +0000

    Dev: near first release, just new dir maker

--- src/main.rs
+++ src/main.rs
@@ -2,73 +2,96 @@
 
 use eframe::egui;
 use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 use std::fs::File;
 use std::io;
 use std::io::BufReader;
 use std::io::Write;
 use std::path::Path;
+use std::path::PathBuf;
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize)]
 struct KaemojiData {
     emoticon: String,
-    kind: String,
     tags: Option<Vec<String>>,
 }
 
 impl KaemojiData {
     fn new() -> Self {
         KaemojiData {
+            emoticon: String::new(),
+            tags: None,
+        }
+    }
+}
+
+impl Default for KaemojiData {
+    fn default() -> Self {
+        KaemojiData {
             emoticon: "( ͡° ͜ʖ ͡°)".to_string(),
-            kind: "Example kaemoji".to_string(),
             tags: Some(["example".to_string()].to_vec()),
         }
     }
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize)]
 struct KaemojiConfig {
-    kaemojis: Vec<KaemojiData>,
+    kaemojis: HashMap<String, Vec<KaemojiData>>,
 }
 
 impl KaemojiConfig {
     fn new() -> Self {
         KaemojiConfig {
-            kaemojis: vec![KaemojiData::new()],
+            kaemojis: HashMap::from([(String::new(), vec![KaemojiData::new()])]),
+        }
+    }
+}
+
+impl Default for KaemojiConfig {
+    fn default() -> Self {
+        KaemojiConfig {
+            kaemojis: HashMap::from([(
+                "Example kaemoji".to_string(),
+                vec![KaemojiData::default()],
+            )]),
         }
     }
 }
 
 fn main() {
-    let _ = render_app();
-    let test = KaemojiConfig::new();
-    let ser_test = serde_json::to_string(&test).unwrap();
-    println!("{}", ser_test);
-    //let des_test: KaemojiData = serde_json::from_str(&ser_test).unwrap();
-    let des_test: KaemojiData = read_file("test.json");
-    //    let des_test: Kaemojis = serde_json::from_str(&ser_test).unwrap();
-    println!("{:?}", des_test)
+    #[cfg(not(any(target_os = "macos", target_os = "ios")))]
+    let config_path: PathBuf = dirs::config_dir().unwrap();
+    #[cfg(any(target_os = "macos", target_os = "ios"))]
+    let config_path = dirs::home_dir().unwrap().join(".config");
+    let config_path = config_path.join("kaemoji_board/KaemojiConfig.json");
+
+    let config: KaemojiConfig = read_file(config_path.as_path());
+
+    if let Err(error) = egui_render_app(config) {
+        panic!("SHIT, problem rendering app: {:?}", error);
+    }
 }
 
-//fn read_file<P: AsRef<Path>>(path: P) -> Result<KaemojiData, io::Error> {
-fn read_file<P: AsRef<Path> + std::marker::Copy>(path: P) -> KaemojiData {
-    match File::open(path) {
+//fn read_file<P: AsRef<Path>>(path: P) -> Result<KaemojiData, io::Error> {  TODO: error handling
+fn read_file<P: AsRef<Path>>(path: P) -> KaemojiConfig {
+    match File::open(&path) {
         Ok(file) => serde_json::from_reader(BufReader::new(file)).unwrap(),
         Err(error) if error.kind() == io::ErrorKind::NotFound => {
             File::create(path)
                 .unwrap()
                 .write_all(
-                    serde_json::to_string_pretty(&KaemojiData::new())
+                    serde_json::to_string_pretty(&KaemojiConfig::default())
                         .unwrap()
                         .as_ref(),
                 )
                 .unwrap();
-            KaemojiData::new()
+            KaemojiConfig::default()
         }
-        Err(error) => panic!("SHIT, Problem reading file: {:?}", error),
+        Err(error) => panic!("SHIT, problem reading file: {:?}", error),
     }
 }
 
-fn render_app() -> Result<(), eframe::Error> {
+fn egui_render_app(uidata: KaemojiConfig) -> Result<(), eframe::Error> {
     let options = eframe::NativeOptions {
         initial_window_size: Some(egui::vec2(240.0, 120.0)),
         ..Default::default()
@@ -78,31 +101,31 @@ fn render_app() -> Result<(), eframe::Error> {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading("Kaemoji board");
 
-            egui::ScrollArea::vertical().show(ui, |ui| {
-                ui.with_layout(
-                    egui::Layout::with_main_wrap(
-                        // adds the button wrapping
-                        egui::Layout::left_to_right(egui::Align::TOP), // sets the button alignment
-                        // to be left to right
-                        true,
-                    ),
-                    |ui| {
-                        let var = [0, 1, 2];
-                        for i in var {
-                            if ui.button(var[i].to_string()).clicked() {
-                                ui.output_mut(|o| o.copied_text = var[i].to_string()); // copies
-                                                                                       // the text
-                                                                                       // of the
-                                                                                       // current
-                                                                                       // kaemoji
-                                                                                       // to
-                                                                                       // clipboard
-                                frame.close()
-                            }
-                        }
-                    },
-                );
-            });
+            for (heading, kaemojis) in uidata.kaemojis.iter() {
+                ui.collapsing(heading, |ui| {
+                    egui::ScrollArea::vertical().show(ui, |ui| {
+                        ui.with_layout(
+                            egui::Layout::with_main_wrap(
+                                // adds the button wrapping
+                                egui::Layout::left_to_right(egui::Align::TOP), // sets the button alignment
+                                // to be left to right
+                                true,
+                            ),
+                            |ui| {
+                                for kaemoji in kaemojis {
+                                    if ui.button(&kaemoji.emoticon).clicked() {
+                                        ui.output_mut(|o| {
+                                            o.copied_text = kaemoji.emoticon.clone()
+                                            // copies the text of the current kaemoji to clipboard
+                                        });
+                                        frame.close() // closes app
+                                    }
+                                }
+                            },
+                        );
+                    });
+                });
+            }
         });
     })
 }
